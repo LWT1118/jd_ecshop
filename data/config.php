2bce3d6e4c0cfdde8f2bed68ffbb0c398bb34e0d
liuweitao <251278498@qq.com>
2017-02-04 23:34:15 +0800

    修改预览图片为150

--- data/config.php
+++ data/config.php
@@ -38,7 +38,7 @@ define('AUTH_KEY', 'this is a key');
 
 define('OLD_AUTH_KEY', '');
 
-define('API_TIME', '2017-02-03 08:10:05');
+define('API_TIME', '2017-02-04 22:53:32');
 
 define('DEBUG_MODE', 4);
 ?>
